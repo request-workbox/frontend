c3975f23ea4968d5d544917e748fd3eb58605580
Zach <zacharycoss@gmail.com>
2020-06-03 08:41:34 -0500

    drafting edit request component

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,3 @@
     <router-view/>
   </div>
 </template>
-
-<style lang="scss">
-#app {
-    top: 0;
-    bottom:0;
-    left: 0;
-    right: 0;
-    position: absolute;
-    display: flex;
-    background: rgba(29, 30, 33, 1);
-}
-</style>
--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,14 +1,32 @@
 <template>
-  <CreateRequest />
+  <div id="container">
+    <CreateRequest />
+    <EditRequest />
+  </div>
+  
 </template>
 
 <script>
 import CreateRequest from './CreateRequest'
+import EditRequest from './EditRequest'
 
 export default {
   name: 'Dashboard',
   components: {
     CreateRequest,
+    EditRequest,
   }
 }
 </script>
+
+<style lang="scss">
+#container {
+    top: 0;
+    bottom:0;
+    left: 0;
+    right: 0;
+    position: absolute;
+    display: flex;
+    background: rgba(29, 30, 33, 1);
+}
+</style>
