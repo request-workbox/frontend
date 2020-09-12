bcbef61e61be1212d9bcf362ce165422c04578ec
Zach <zacharycoss@gmail.com>
2020-09-12 03:28:29 -0500

    refactoring stat storage

--- src/components/StatisticOptionsDetails.vue
+++ src/components/StatisticOptionsDetails.vue
@@ -2,22 +2,26 @@
   <div class="row">
     <div class="column column-full-width">
 
-    
-
     <div class="row row-border-bottom">
-      <div class="column column-data column-header column-10">Status Code</div>
-      <div class="column column-data column-header column-10">Status Text</div>
-      <div class="column column-data column-header column-10">Request Name</div>
-      <div class="column column-data column-header column-20">Request Type</div>
-      <div class="column column-data column-header column-grow">Date</div>
+      <div class="column column-data column-header column-10 column-padded">Status Code</div>
+      <div class="column column-data column-header column-10 column-padded">Status Text</div>
+      <div class="column column-data column-header column-10 column-padded">Request Name</div>
+      <div class="column column-data column-header column-20 column-padded">Request Type</div>
+      <div class="column column-data column-header column-grow column-padded">Date</div>
     </div>
 
     <div class="row row-border-bottom table-row-selectable" v-bind:class="{'table-row-selected':shouldBeSelected(stat._id)}" v-for="(stat) in this.selectedData().stats" :key="stat._id" v-on:click="selectStatAction(stat)">
-      <div class="column column-data column-10">{{ stat.status }}</div>
-      <div class="column column-data column-10">{{ stat.statusText }}</div>
-      <div class="column column-data column-10">{{ stat.requestName }}</div>
-      <div class="column column-data column-20">{{ stat.requestType }}</div>
-      <div class="column column-data column-grow">{{ statisticCreatedAt(stat.createdAt) }}</div>
+      <div class="column column-data column-10 column-padded">{{ stat.status }}</div>
+      <div class="column column-data column-10 column-padded">{{ stat.statusText }}</div>
+      <div class="column column-data column-10 column-padded">{{ stat.requestName }}</div>
+      <div class="column column-data column-20 column-padded">{{ stat.requestType }}</div>
+      <div class="column column-data column-grow column-padded">{{ statisticCreatedAt(stat.createdAt) }}</div>
+    </div>
+
+    <div class="row row-border-bottom" v-if="shouldShowSelectedStat()">
+      <div class="column filter-button filter-button-left filter-button-active">Stats</div>
+      <div class="column filter-button">Request</div>
+      <div class="column filter-button filter-button-right">Response</div>
     </div>
 
     <pre v-if="shouldShowSelectedStat()">
