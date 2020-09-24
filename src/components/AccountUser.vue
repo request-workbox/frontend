2f875054d798cc5a942ca60bdd78ab0bf2d0377b
Zach <zacharycoss@gmail.com>
2020-09-24 14:33:36 -0500

    drafting sign up and log in pages

--- src/components/AccountUser.vue
+++ src/components/AccountUser.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="row">
     <div class="column column-full-width">
+
+      <!-- Contact Information -->
+
       <div class="row row-border-bottom">
         <div class="column column-data column-header-text column-grow column-group-header">Contact Information</div>
       </div>
@@ -17,6 +20,8 @@
         </div>
         <span class="tiny-text tiny-text-spaced">zacharycoss@gmail.com</span>
       </div>
+
+      <!-- Reset Password -->
 
       <div class="row row-border-bottom">
         <div class="column column-data column-header-text column-grow column-group-header">Reset Password</div>
@@ -58,6 +63,84 @@
       <div class="row row-border-bottom">
         <div class="column column-data column-20">
           <div class="column text-button action action-text-center">Reset</div>
+        </div>
+      </div>
+
+      <!-- Signup -->
+
+      <div class="row row-border-bottom">
+        <div class="column column-data column-header-text column-grow column-group-header">Sign Up</div>
+      </div>
+
+      <div class="row row-border-bottom">
+        <div class="column column-data column-20">
+            <input type="text" class="column-input-text" disabled value="Email">
+          </div>
+          <div class="column column-data column-grow">
+            <input type="text" placeholder="Email" class="column-input-text">
+          </div>
+      </div>
+
+      <div class="row row-border-bottom">
+        <div class="column column-data column-20">
+            <input type="text" class="column-input-text" disabled value="Username">
+          </div>
+          <div class="column column-data column-grow">
+            <input type="text" placeholder="Username" class="column-input-text">
+          </div>
+      </div>
+
+      <div class="row row-border-bottom">
+        <div class="column column-data column-20">
+            <input type="text" class="column-input-text" disabled value="Password">
+          </div>
+          <div class="column column-data column-grow">
+            <input type="text" placeholder="Password" class="column-input-text">
+          </div>
+      </div>
+
+      <div class="row row-border-bottom">
+        <div class="column column-data column-20">
+            <input type="text" class="column-input-text" disabled value="Password">
+          </div>
+          <div class="column column-data column-grow">
+            <input type="text" placeholder="Confirm Password" class="column-input-text">
+          </div>
+      </div>
+
+      <div class="row row-border-bottom">
+        <div class="column column-data column-20">
+          <div class="column text-button action action-text-center">Sign Up</div>
+        </div>
+      </div>
+
+      <!-- Log In -->
+
+      <div class="row row-border-bottom">
+        <div class="column column-data column-header-text column-grow column-group-header">Log In</div>
+      </div>
+
+      <div class="row row-border-bottom">
+        <div class="column column-data column-20">
+            <input type="text" class="column-input-text" disabled value="Username">
+          </div>
+          <div class="column column-data column-grow">
+            <input type="text" placeholder="Username" class="column-input-text">
+          </div>
+      </div>
+
+      <div class="row row-border-bottom">
+        <div class="column column-data column-20">
+            <input type="text" class="column-input-text" disabled value="Password">
+          </div>
+          <div class="column column-data column-grow">
+            <input type="text" placeholder="Password" class="column-input-text">
+          </div>
+      </div>
+
+      <div class="row row-border-bottom">
+        <div class="column column-data column-20">
+          <div class="column text-button action action-text-center">Log In</div>
         </div>
       </div>
 
