480c51af90eca171b411c4b66b5c92af0f0b6240
Zach <zacharycoss@gmail.com>
2020-09-28 11:28:14 -0500

    moving header into component file

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,18 @@
 <template>
   <div id="app">
-    <div class="row row-border-bottom" id="site-header-container">
-      <div class="column column-full-width">
-        <div class="row">
-          <div class="column text-button" id="site-header">Request Workbox</div>
-          <div class="column column-grow"></div>
-          <div class="column text-button" id="support-button">Support</div>
-          <div class="column text-button" id="feedback-button">Feedback</div>
-        </div>
-      </div>
-    </div>
+    <Header />
     <router-view />
   </div>
 </template>
 
 <script>
+import Header from './components/Header'
 
 export default {
   name: "App",
+  components: {
+    Header,
+  },
   mounted: async function() {
     try {
       if (!this.$route.name) {
@@ -44,23 +39,6 @@ body {
 
 #app {
   font-family: "Roboto", sans-serif;
-}
-#site-header-container {
-  background: black;
-}
-#site-header {
-  font-size: 17px;
-  font-weight: 700;
-  background: transparent;
-  color: white;
-}
-#support-button {
-  background: transparent;
-  color: white;
-}
-#feedback-button {
-  background: transparent;
-  color: white;
 }
 
 @import './src/styles/main';
---
+++ src/components/Header.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="row row-border-bottom" id="site-header-container">
+        <div class="column column-full-width">
+        <div class="row">
+            <div class="column text-button" id="site-header">Request Workbox</div>
+            <div class="column column-grow"></div>
+            <div class="column text-button" id="support-button">Support</div>
+            <div class="column text-button" id="feedback-button">Feedback</div>
+        </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: "Header",
+};
+</script>
+
+<style lang="scss">
+#site-header-container {
+  background: black;
+}
+#site-header {
+  font-size: 17px;
+  font-weight: 700;
+  background: transparent;
+  color: white;
+}
+#support-button {
+  background: transparent;
+  color: white;
+}
+#feedback-button {
+  background: transparent;
+  color: white;
+}
+</style>
